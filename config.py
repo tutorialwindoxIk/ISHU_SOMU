c47eaeff0847209ff4e0ca6516fe229d10737307
Ishu_official <somnathhor12052005@gmail.com>
2024-09-23 19:22:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WCGKING/BRANDEDKING",
+    "https://github.com/tutorialwindoxIk/ISHU_SOMU",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
